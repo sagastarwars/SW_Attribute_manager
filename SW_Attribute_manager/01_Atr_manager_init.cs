f842ebb16e57d9b39b4b8340a11dd30f7825bf84
AVazianskii <avazianskii@mail.ru>
2022-02-16 22:07:51 +0300

    Добавьте файлы проекта.

---
+++ SW_Attribute_manager/01_Atr_manager_init.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attribute_libs
+{
+    public partial class Attribute_manager
+    {
+    }
+}
